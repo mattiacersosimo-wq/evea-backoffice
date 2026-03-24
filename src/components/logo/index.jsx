606b2f32602b37e676b3e297fa1cb84bf1b413b2
Mattia Cersosimo <mattiacersosimo@gmail.com>
2026-03-24 21:46:10 +0100

    fix: center logo in sidebar

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -8,7 +8,7 @@ const Logo = ({ disabledLink = false }) => {
   if (disabledLink) {
     return (
       <img
-        style={{ width: "100px", height: "auto", maxHeight: "64px" }}
+        style={{ width: "100px", height: "auto", display: "block", margin: "0 auto", maxHeight: "64px" }}
         src={logo}
       />
     );
@@ -16,7 +16,7 @@ const Logo = ({ disabledLink = false }) => {
 
   return (
     <RouterLink to="/">
-      <img style={{ width: "100px", height: "auto" }} src={logo} />
+      <img style={{ width: "100px", height: "auto", display: "block", margin: "0 auto" }} src={logo} />
     </RouterLink>
   );
 };
