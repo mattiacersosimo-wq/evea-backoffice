ba534493a0c2949b3ca38b59cc728062381b0c76
Mattia Cersosimo <mattiacersosimo@gmail.com>
2026-03-24 21:37:44 +0100

    fix: logo white background to match sidebar

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -8,7 +8,7 @@ const Logo = ({ disabledLink = false }) => {
   if (disabledLink) {
     return (
       <img
-        style={{ width: "100px", height: "auto", maxHeight: "64px" }}
+        style={{ width: "100px", height: "auto", backgroundColor: "#fff", borderRadius: 4, padding: 4, maxHeight: "64px" }}
         src={logo}
       />
     );
@@ -16,7 +16,7 @@ const Logo = ({ disabledLink = false }) => {
 
   return (
     <RouterLink to="/">
-      <img style={{ width: "100px", height: "auto" }} src={logo} />
+      <img style={{ width: "100px", height: "auto", backgroundColor: "#fff", borderRadius: 4, padding: 4 }} src={logo} />
     </RouterLink>
   );
 };
